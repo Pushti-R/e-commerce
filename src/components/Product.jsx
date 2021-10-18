0eb76644ad9c3b4e85446c7cca7f05ab2afbc22f
Pushti <pushti.ratanghayra@gmail.com>
2021-10-18 22:20:01 +0530

    c2

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -33,13 +33,13 @@ const Container = styled.div`
     }
 `
 
-const Circle = styled.div`
+/*const Circle = styled.div`
     width: 200px;
     height: 200px;
     border-radius: 50%;
     background-color: white;
     position: absolute;
-`
+`*/
 
 const Image = styled.img`
     height: 75%;
